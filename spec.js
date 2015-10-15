53f78c1b59956ec4a68635a4e59de035d5091a7e
Kai <riselikethemoon@gmail.com>
2015-10-15 14:09:47 -0700

    copied test code also doesn't run :(

--- spec.js
+++ spec.js
@@ -1,38 +1,58 @@
+// beforeEach(function() {
+// 	this.addMatchers({
+
+// 		toBeFifty: function() {
+// 			this.message = function() {
+// 				return "Expected " + this.actual + " to be 50";
+// 			};
+// 			return this.actual === 50;
+// 		}
+
+// 	});
+// });
+
 beforeEach(function() {
-	this.addMatchers({
-		toBeFifty: function() {
-			this.message = function() {
-				return "Expected " + this.actual + " to be 50";
-			};
-			return this.actual === 50;
-		}
-	});
+  this.addMatchers({
+      toBeLarge: function() {
+          this.message = function() {
+              return "Expected " + this.actual + " to be large";
+          };
+          return this.actual > 100;
+      }
+  });
 });
 
+// expect(200).toBeLarge();
 
 
-describe("Tester", function() {
+describe("Custom Test", function() {
 	it("runs!", function() {
-		expect(testMe()).toEqual("I work!");
+		expect(200).toBeLarge();
 	});
 });
 
-describe("Adder", function() {
-	it("adds positive numbers", function() {
-		expect(adder(3, 4)).toEqual(7);
-	});
+// describe("Tester", function() {
+// 	it("runs!", function() {
+// 		expect(testMe()).toEqual("I work!");
+// 	});
+// });
 
-	it("adds negative numbers", function() {
-		expect(adder(-1, 0)).toEqual(-1);
-	});
+// describe("Adder", function() {
+// 	it("adds positive numbers", function() {
+// 		expect(adder(3, 4)).toEqual(7);
+// 	});
 
-	it("doesn't equal the wrong thing", function() {
-		expect(adder(4,2)).not.toEqual(0);
-	});
+// 	it("adds negative numbers", function() {
+// 		expect(adder(-1, 0)).toEqual(-1);
+// 	});
 
-	it("gets close to the right answer", function() {
-		expect(adder(12, 11.5)).toBeCloseTo(23.54, 1);
-	});
+// 	it("doesn't equal the wrong thing", function() {
+// 		expect(adder(4,2)).not.toEqual(0);
+// 	});
+
+// 	it("gets close to the right answer", function() {
+// 		expect(adder(12, 11.5)).toBeCloseTo(23.54, 1);
+// 	});
 
 	// it("breaks when passed string", function() {
 	// 	expect(function() {
@@ -40,35 +60,35 @@ describe("Adder", function() {
 	// 	}).toThrow();
 	// });
 
-	it("uses toThrow", function() {
-		expect(function() {
-		    calculate("string");
-		}).toThrow();
-	});
+	// it("uses toThrow", function() {
+	// 	expect(function() {
+	// 	    calculate("string");
+	// 	}).toThrow();
+	// });
 
 	// it("uses a custom matcher", function() {
 	// 	expect(adder(25, 25)).toBeFifty();
 	// 	});
 	// });
 
-});
+// });
 
-describe("Greeter", function() {
-	it("greets name", function() {
-		expect(greet("Atlas")).toEqual("Hello Atlas");
-	});
+// describe("Greeter", function() {
+// 	it("greets name", function() {
+// 		expect(greet("Atlas")).toEqual("Hello Atlas");
+// 	});
 
-	it("works without a name argument", function() {
-		expect(greet()).toContain("name?");
-	});
+// 	it("works without a name argument", function() {
+// 		expect(greet()).toContain("name?");
+// 	});
 
-	it("matches a regex", function() {
-		expect(greet("Ari")).toMatch(/lo/);
-	});
-});
+// 	it("matches a regex", function() {
+// 		expect(greet("Ari")).toMatch(/lo/);
+// 	});
+// });
 
-describe("Appender", function() {
-	it("contains the new word", function() {
-		expect(appendWord("cherry")).toContain("cherry");
-	});
-});
+// describe("Appender", function() {
+// 	it("contains the new word", function() {
+// 		expect(appendWord("cherry")).toContain("cherry");
+// 	});
+// });
